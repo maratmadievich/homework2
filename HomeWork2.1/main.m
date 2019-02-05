47a5a0b89e1262948b1bf172498f97e72a317995
Admin <admin@iMac-Admin.local>
2019-02-05 14:10:35 +0600

    Initial Commit

---
+++ HomeWork2.1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HomeWork2.1
+//
+//  Created by Admin on 2/5/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
